4eab654a5585b65d83ae50d150b7dea40c031532
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-22 23:40:04 +0530

    Implement about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const about = () => {
   return (
-    <div>about</div>
-  )
-}
+    <Layout>
+      <section>
+        <div className="container">
+          <h1>About Crypto Trade</h1>
+          <p>
+            Crypto Trade is a website that allows you to track the price of cryptocurrencies.
+            You can also buy and sell cryptocurrencies.
+          </p>
+        </div>
+      </section>
+    </Layout>
+  );
+};
 
 export default about
